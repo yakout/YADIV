4f9e59f33e71b7df6deee41c483057defe651dfe
Youssef-Darwish <yousef952009@hotmail.com>
2017-10-26 01:06:46 +0200

    Adding action and slots

--- imageviewer.cpp
+++ imageviewer.cpp
@@ -1,6 +1,7 @@
 #include "imageviewer.h"
 #include "ui_imageviewer.h"
-
+#include "QMessageBox"
+#include "QFileDialog"
 ImageViewer::ImageViewer(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::ImageViewer)
@@ -13,4 +14,40 @@ ImageViewer::ImageViewer(QWidget *parent) :
 ImageViewer::~ImageViewer()
 {
     delete ui;
+}
+
+
+void ImageViewer::on_actionopen_triggered()
+{
+    QString fileName = QFileDialog::getOpenFileName(this,
+           tr("Choose image to view"), "",
+           tr("Address Book (*.abk);;All Files (*)"));
+    //a comment
+}
+
+void ImageViewer::on_actionsave_triggered()
+{
+
+}
+
+void ImageViewer::on_actionReset_triggered()
+{
+
+}
+
+void ImageViewer::on_actionZoom_In_triggered()
+{
+
+}
+
+void ImageViewer::on_actionZoom_Out_triggered()
+{
+
+}
+
+void ImageViewer::on_actionabout_triggered()
+{
+    QMessageBox::about(this, tr("About Image Viewer"),
+               tr("<p>The <b>Image Viewer</b> is an image editor written in Qt C++ that "
+               "enables the user to view and modify images in different formats .</p>"));
 }
--- imageviewer.h
+++ imageviewer.h
@@ -2,6 +2,13 @@
 #define IMAGEVIEWER_H
 
 #include <QMainWindow>
+#include <QWidget>
+#include <QMap>
+
+class QPushButton;
+class QLabel;
+class QLineEdit;
+class QTextEdit;
 
 namespace Ui {
 class ImageViewer;
@@ -11,12 +18,29 @@ class ImageViewer : public QMainWindow
 {
     Q_OBJECT
 
+
+public slots:
+
+    //void openFile();
+
+
+
 public:
     explicit ImageViewer(QWidget *parent = 0);
     ~ImageViewer();
 
+private slots:
+
+    void on_actionopen_triggered();
+    void on_actionsave_triggered();
+    void on_actionReset_triggered();
+    void on_actionZoom_In_triggered();
+    void on_actionZoom_Out_triggered();
+    void on_actionabout_triggered();
+
 private:
     Ui::ImageViewer *ui;
+     QPushButton *addButton;
 };
 
 #endif // IMAGEVIEWER_H
